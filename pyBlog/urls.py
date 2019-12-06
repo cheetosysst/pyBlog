2c042e04b635b1f7f3693e034b5c9b3223f11e37
cheetosysst <x9930707@gmail.com>
2019-12-06 12:36:02 +0800

    change url

--- pyBlog/urls.py
+++ pyBlog/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('index.urls')),
+    path('/', include('index.urls')),
     path('post/', include('post.urls'))
 ]
